9edef28eb56130abeb8a0af0997f7b093ceb9c4a
JamesCosta1 <jamesaaroncosta@gmail.com>
2023-02-26 00:30:10 -0700

    furthered main file and partitioned into helper

--- helper_functions.py
+++ helper_functions.py
@@ -1,6 +1,10 @@
 #! /usr/bin/python3
 
-def get_user_int(input_: any) -> int:
+from time import sleep
+from amortization_functons import calculate_payment_amount, amortizatize
+
+
+def read_user_int(input_: any) -> int:
     while True:
         input_ = input("")
         try:
@@ -12,7 +16,7 @@ def get_user_int(input_: any) -> int:
 
 
 
-def get_user_float(input_: any) -> float:
+def read_user_float(input_: any) -> float:
     while True:
         input_ = input("")
         try:
@@ -21,3 +25,23 @@ def get_user_float(input_: any) -> float:
 
         except ValueError:
           print('Please enter a valid input.')
+
+
+def recieve_initial_paramaters() -> tuple(float, float, int, float):
+    print('Please enter the size of the loan (to the nearest cent)')
+    sleep(3)
+    princpial = read_user_float()
+    
+    print('Please enter the loan\'s annual interest rate (as a decimal).')
+    sleep(3)
+    annual_interest_rate = read_user_float()
+
+    print('Please enter the lifetime of the loan be (in months)')
+    sleep(3)
+    number_of_payments = read_user_int()
+
+    monthly_payment_amount = calculate_payment_amount(princpial, annual_interest_rate, number_of_payments)
+
+    print(f'Thank you. Your monthly payment will be ${monthly_payment_amount} for a total of{number_of_payments} months.')
+
+    return princpial, annual_interest_rate, number_of_payments, monthly_payment_amount
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 #! /usr/bin/python3
 
 from os import system
-
-
-from amortization_functons import calculate_payment_amount
+import helper_functions as hf
+from plotting_functions import plotter
 
 def main() -> None:
     system('clear')
@@ -13,7 +12,43 @@ def main() -> None:
         print("Please select an option below:")
         print("1. Calculate my monthly payment")
         print("0. Close progam")
-        userinput = input()
+        
+        input_ = hf.read_user_int()
+        if input_ == 0:
+            program_is_running = False
+        elif input_ == 1:
+            princpial, annual_interest_rate, number_of_payments, monthly_payment_amount = hf.recieve_initial_paramaters()
+
+            while not monthly_payment_amount is None:
+                print('What would you like to do now?')
+                print('1. Display the monthly schedule using a monthly payment of XXXXXX')
+                print('2. View the amortization graph using a monthly payment of XXXXXX')
+                print('3. To back to main menu.')
+                print('0. Exit')
+
+                input_ = hf.read_user_int()
+
+                if input_ == 0:
+                    program_is_running = False
+                    break
+                elif input_ == 1:
+                    # DISPLAY THE MONTHLY SCHEDULE
+                    pass
+
+                elif input_ == 2:
+                    # Grab the lists that need placed into the plotter.
+                    balances, total_interest_paid, total_principal_paid, monthly_interest_paid, monthly_principal_paid = amortizatize(princpial, annual_interest_rate, number_of_payments, monthly_payment_amount)
+
+                elif input_ == 3:
+                    break
+
+                else:
+                    print("Please input a valid option.")
+
+        else:
+            print("Please input a valid option.")
+    
+    print('Thank you, have good day!')
 
 
 
