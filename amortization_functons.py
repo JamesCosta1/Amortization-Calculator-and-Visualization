86435ef18bae363c700a7c0c4c2d0c74af67db48
JamesCosta1 <jamesaaroncosta@gmail.com>
2023-02-28 15:44:45 -0700

    edit plots, add data display, add docstr

--- amortization_functons.py
+++ amortization_functons.py
@@ -2,19 +2,24 @@
 import numpy as np
 from decimal import Decimal
 from tabulate import tabulate
+import pandas as pd
 
-def calculate_payment_amount(principal: float, annual_interest_rate: float, number_of_payments: int) -> float:
+def calculate_payment_amount(principal: float,
+                             annual_interest_rate: float,
+                             number_of_payments: int) -> float:
     '''
+    Calculates the monthly payment amount in order to amortizate a loan.
+    
     Args:
         principal (float):              The total amount of money lent
-        annual_interest_rate (float) :  The percentage amount the owed amount will grow in a year
+        annual_interest_rate (float):   The percentage amount the owed amount will grow in a year
         number_of_payments (int):       The total number of payments made over the life of the loan
                                         (If payments are made monthly, this is the same value as the
                                         loan's lifetime in months)
 
 
     Returns:
-        Monthly payment (float)
+        monthly_payment_amount (float)
     '''
     # Convert annual interest rate into monthly
     interest_rate = annual_interest_rate / 12
@@ -31,7 +36,33 @@ def calculate_payment_amount(principal: float, annual_interest_rate: float, numb
 
 
 
-def amortizatize(principal: float, annual_interest_rate: float, monthly_payment_amount: float) -> tuple[list[float], list[float], list[float], list[float], list[float]]:
+def amortizatize(principal: float,
+                 annual_interest_rate: float,
+                 monthly_payment_amount: float) -> tuple[list[float],
+                                                         list[float],
+                                                         list[float],
+                                                         list[float],
+                                                         list[float]]:
+
+    '''
+    Args:
+        principal (float):                    The total amount of money lent
+        annual_interest_rate (float) :        The percentage amount the owed amount will grow in a year
+        monthly_payment_amount (float):       The amount of money paid per month
+
+
+    Returns:
+        balances (list[float]):               The amount of money still owed each month
+        total_interest_paid (list[float]):    The amount of interest paid thus far
+        total_principal_paid (list[float]):   The amount of principal paid thus far
+        monthly_interest_paid (list[float]):  The amount of funds used to pay interest each month
+        monthly_principal_paid (list[float]): The amount of funds used to pay interest each month
+
+
+
+    '''
+
+    # Initialize
     remaining_balance = principal
     balances = [principal]
     total_interest_paid = [0]
@@ -47,8 +78,10 @@ def amortizatize(principal: float, annual_interest_rate: float, monthly_payment_
 
         # Calculate the interest amount for this month
         interest_amount = remaining_balance * interest_rate
+
         # Calculate balance after interest
         remaining_balance *= (1 + interest_rate)
+
         # Make the monthly payment
         remaining_balance -= monthly_payment_amount
 
@@ -67,53 +100,36 @@ def amortizatize(principal: float, annual_interest_rate: float, monthly_payment_
     total_interest_paid = [round(x, 2) for x in total_interest_paid]
     total_principal_paid = [round(x, 2) for x in total_principal_paid]
     monthly_interest_paid = [round(x, 2) for x in monthly_interest_paid]
-    monthly_principal_paid = [round(x,2) for x in monthly_principal_paid]
+    monthly_principal_paid = [round(x, 2) for x in monthly_principal_paid]
 
     return balances, total_interest_paid, total_principal_paid, monthly_interest_paid, monthly_principal_paid
 
-# Testing the amortizate function:
-def testing_function(): # Delete later!!!!!!
 
+def monthly_exhibition(balances,
+                       total_interest_paid,
+                       total_principal_paid,
+                       monthly_interest_paid,
+                       monthly_principal_paid) -> None:
+
+    '''
+    Displays on a monthly basis (1) remaining balance, (2) total interest paid, (3) total Principal paid, (3) monthly interest paid, and 
+    (3) monthly principal paid. 
+    '''
+
+
+    data = {
+        'Remaining Balance': balances,
+        'Total Interest Paid': total_interest_paid,
+        'Total Principal Paid': total_principal_paid,
+        'Monthly Interest Paid': monthly_interest_paid,
+        'Monthly Principal Paid': monthly_principal_paid
+        }
+
+
+    df = pd.DataFrame(data)
+
+    print(df)
+
+if __name__ == '__main__':
     balances, total_interest_paid, total_principal_paid, monthly_interest_paid, monthly_principal_paid = amortizatize(5_000, 0.05, 219.36)
-
-    
-
-    print(f'The balances at the end of every month is:{balances}')
-
-    print('')
-
-    print(f'The  total interest paid thusfar is:{total_interest_paid}')
-
-    print('')
-
-    print(f'The total principal paid paid thusfar is:{total_principal_paid}')
-
-    print('')
-
-    print(f'The amount of interest paid at the end of every month is:{monthly_interest_paid}')
-
-    print('')
-
-    print(f'The amount of principal paid at the end of every month is:{monthly_principal_paid}')
-
-
-# testing_function()
-    
-
-def monthly_exhibition(balances, total_interest_paid, total_principal_paid, monthly_interest_paid, monthly_principal_paid) -> None:
-    '''
-    Displays the status of funds on a monthly basis
-    '''
-
-    table_data = np.array([balances, total_interest_paid, total_principal_paid, monthly_interest_paid, monthly_principal_paid])
-    headers = ['Remaining Balance', 'Total Interest Paid', 'Total Principal Paid', 'Monthly Interest Paid', 'Monthly Principal Paid']
-
-    # Generate the table
-    table = tabulate(table_data, headers, tablefmt='fancygrid')
-    print(table)
-
-
-balances, total_interest_paid, total_principal_paid, monthly_interest_paid, monthly_principal_paid = amortizatize(5_000, 0.05, 219.36)
-monthly_exhibition(balances, total_interest_paid, total_principal_paid, monthly_interest_paid, monthly_principal_paid)
-
-# This data is printing the data in a hideous way. May need to use something differnet or edit what I have
+    monthly_exhibition(balances, total_interest_paid, total_principal_paid, monthly_interest_paid, monthly_principal_paid)
